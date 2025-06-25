c1b34eeb715aa30740ceecfd6c09a2130699337f
JoseMAGG <64233535+JoseMAGG@users.noreply.github.com>
2025-06-24 22:01:45 -0500

    Contacts Update LoginTest.scala

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -13,15 +13,31 @@ class LoginTest extends Simulation{
     .check(status.is(200))
 
   // 2 Scenario Definition
-  val scn = scenario("Login").
-    exec(http("login")
+  val scn = scenario("Login")
+    .exec(http("login")
       .post(s"users/login")
       .body(StringBody(s"""{"email": "$email", "password": "$password"}""")).asJson
        //Validar status 200
       .check(status.is(200))
       .check(jsonPath("$.token").saveAs("authToken"))
     )
-
+    .exec(http("contacts")
+      .post(s"contacts")
+      .header("Authorization", "Bearer $authToken")
+      .body(StringBody(s"""{"firstName": "John",
+    "lastName": "Doe",
+    "birthdate": "1970-01-01",
+    "email": "jdoe@fake.com",
+    "phone": "8005555555",
+    "street1": "1 Main St.",
+    "street2": "Apartment A",
+    "city": "Anytown",
+    "stateProvince": "KS",
+    "postalCode": "12345",
+    "country": "USA"}""")).asJson
+       //Validar status 200
+      .check(status.is(201))
+    )
   // 3 Load Scenario
   setUp(
     scn.inject(rampUsersPerSec(5).to(15).during(30))

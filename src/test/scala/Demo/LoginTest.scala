6e476db37e1ab88ec7c568665d39a93b00fbf9fa
JoseMAGG <64233535+JoseMAGG@users.noreply.github.com>
2025-06-28 17:36:18 -0500

    Update LoginTest.scala

--- src/test/scala/Demo/LoginTest.scala
+++ src/test/scala/Demo/LoginTest.scala
@@ -12,13 +12,7 @@ class LoginTest extends Simulation{
     //Verificar de forma general para todas las solicitudes
     .check(status.is(200))
   
-  val customFeeder = Iterator.continually {
-    val rnd = new scala.util.Random
-    Map(
-      "firstName" -> s"First${rnd.nextInt(10000)}",
-      "lastName"  -> s"Last${rnd.nextInt(10000)}",
-    )
-  }
+  val rnd = new scala.util.Random
   
   // 2 Scenario Definition
   val scn = scenario("Login")
@@ -29,12 +23,11 @@ class LoginTest extends Simulation{
       .check(status.is(200))
       .check(jsonPath("$.token").saveAs("authToken"))
     )
-    .feed(customFeeder)
     .exec(
       http("Create Contact")
         .post(s"contacts")
         .header("Authorization", "Bearer ${authToken}")
-        .body(StringBody(s"""{"firstName": "$firstName","lastName": "$lastName","birthdate": "1970-01-01","email": "jdoe@fake.com","phone": "8005555555","street1": "1 Main St.","street2": "Apartment A","city": "Anytown","stateProvince": "KS","postalCode": "12345","country": "USA"}""")).asJson
+        .body(StringBody(s"""{"firstName": "First${rnd.nextInt(10000)}","lastName": "Last${rnd.nextInt(10000)}","birthdate": "1970-01-01","email": "jdoe@fake.com","phone": "8005555555","street1": "1 Main St.","street2": "Apartment A","city": "Anytown","stateProvince": "KS","postalCode": "12345","country": "USA"}""")).asJson
         .check(status.is(201))
     )
   // 3 Load Scenario
